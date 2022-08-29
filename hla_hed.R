4ff1cd212b3263bbcc06932b99448d4e584ace9a
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2022-08-29 21:24:43 +0100

    calculate_distance works

--- hla_hed.R
+++ hla_hed.R
@@ -14,7 +14,7 @@ read_fasta <- function(infile){
   
   for(element in fasta.named.list){
     if(element.size == -1){
-      element.size = length(element)
+      element.size <- length(element)
     }
     else{
       if(element.size != length(element)){
@@ -32,4 +32,29 @@ read_aa <- function(infile){
   setDT(dt)
   
   return(dt)
+}
+
+calculate_distance <- function(hla1, hla2, sequence, distance){
+  seq_hla1 <- sequence[hla1]
+  seq_hla2 <- sequence[hla2]
+
+  if(is.null(seq_hla1[[1]])){
+    stop(cat(hla1, "does not exist in sequence."))
+  }
+
+  if(is.null(seq_hla2[[1]])){
+    stop(cat(hla2, "does not exist in sequence."))
+  }
+
+  seq_len = length(seq_hla1[[1]])
+  dis <- 0
+  
+  for(i in 1:seq_len){
+    aa1 = seq_hla1[[1]][[i]]
+    aa2 = seq_hla2[[1]][[i]]
+    dis = dis + distance[X == toupper(aa1), c(toupper(aa2)), with = FALSE][[1]]
+  }
+
+  dis = dis / seq_len
+  return(dis)
 }
